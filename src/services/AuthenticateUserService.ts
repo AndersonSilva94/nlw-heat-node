5998548205249645e1d4bca4fdd7d6aff145362b
Anderson Silva <andssilva.23@gmail.com>
2021-10-19 16:26:29 -0400

    feat: :rocket: verifica se o usuário existe no db

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import prismaClient from '../prisma';
 // 1 - Receber o código (string)
 // 2 - Recuperar o access_token no github
 // 3 - Recuperar infos do user no github
@@ -38,6 +39,25 @@ class AuthenticateUserService {
         authorization: `Bearer ${accessTokenResponse.access_token}`
       }
     });
+
+    const { login, id, avatar_url, name } = response.data;
+
+    let user = await prismaClient.user.findFirst({
+      where: {
+        github_id: id
+      }
+    });
+
+    if (!user) {
+      user = await prismaClient.user.create({
+        data: {
+          github_id: id,
+          login,
+          avatar_url,
+          name
+        }
+      });
+    }
 
     return response.data;
   }
